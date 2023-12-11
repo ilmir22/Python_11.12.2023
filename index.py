44ef953665d52510ab2af37cdc4619d392014609
ilmir22 <152898248+ilmir22@users.noreply.github.com>
2023-12-11 20:32:41 +0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,93 @@
+# true=True
+# if true == True:
+#     print (f"{true}:True ")
+#     print(type(true))
+    
+# print("1==1", 1==1)
+# print("1==2", 1==2)
+
+# competent = True
+# responsible = False
+# print(competent and responsible) и
+
+# competent = True
+# responsible = False
+# print(competent or responsible) или
+  
+# previously_fired = True
+# print(not previously_fired) отрицание
+
+# speed = int(input("Please enter you speed"))
+# if speed > 200:
+#     print("its very fast") если скорость больше 200
+# else:
+#     print("You are too slow") если скорость меньше 200  
+ 
+# speed = int(input("Please enter you speed"))
+# if speed > 200:
+#     print("its very fast") 
+# elif 100 < speed <200:
+#     if speed == 160:
+#         print("You folow the recommendatoin")
+#     print("Its common speed")    
+# else:
+#     print("You are too slow")
+
+# num=int(input("Введите число: "))
+# if num % 2 == 0:
+#     print("Четное")
+# else:
+#     print("нечетное") 
+
+    
+# num=int(input("Введите число: "))
+# if num % 7 == 0:
+#     print("Number is  multiple 7")
+# else: 
+#     print("Number is not multiple 7")
+
+# num_1 = int(input("Введите первое число: "))
+# num_2 = int(input("Введите второе число: "))
+# if num_1 > num_2:
+#     print(num_1)
+# else:
+#     print(num_2)
+
+# num_1 = int(input("Введите первое число: "))
+# num_2 = int(input("Введите второе число: "))
+# if num_1 < num_2:
+#     print(num_1)
+# else:
+#     print(num_2)
+
+# first_num = int(input("Введите первое число: "))
+# second_num = int(input("Введите второе число: "))
+# print(f"сумма чисел:{first_num+second_num}" )
+
+# first_num = int(input("Введите первое число: "))
+# second_num = int(input("Введите второе число: "))
+# print(f"разница чисел:{first_num-second_num}" )
+
+# first_num = int(input("Введите первое число: "))
+# second_num = int(input("Введите второе число: "))
+# var = (first_num+second_num) / 2
+# print(var)
+
+# first_num = int(input("Введите первое число: "))
+# second_num = int(input("Введите второе число: "))
+# print(f"произведение чисел:{first_num*second_num}" )
+
+# def calculator():
+#     user = input("Введите +, -, * : ")
+#     num_1 = int(input("Введите первое число: "))
+#     num_2 = int(input("Введите второе число: "))
+    
+#     if user == "+":
+#         print(f"Сумма равна {num_1+num_2}")
+#     elif user == "-":
+#         print(f"Сумма равна {num_1-num_2}")
+#     elif user == "*":  
+#         print(f"Сумма равна {num_1*num_2}")
+    
+
+# calculator()
